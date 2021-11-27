804823ae091156d0009fceb8998a9d91727d1b03
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-11-27 11:24:43 +0530

    [Vaibhav Aher]Added:Register API Created

---
+++ RegisterAPI/FundooModelJS/RegisterModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace FundooModelJS
+{
+    public class RegisterModel
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [Required]
+        [RegularExpression("",ErrorMessage="E-mail is not valid")]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+    }
+}
---
+++ RegisterAPI/FundooRepository/Migrations/20211123140716_Fundoo.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FundooRepository.Migrations
+{
+    public partial class Fundoo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
